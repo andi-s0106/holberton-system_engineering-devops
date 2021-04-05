7a8ba84600811051c335b9c8cdc39406f66a6191
andi-s0106 <andress.933110@gmail.com>
2021-04-05 19:52:02 +0000

    updated 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 #  kills a process named killmenow
 exec { 'pkill killmenow':
   command => 'pkill killmenow',
-  path    => '/usr/local/bin/:/bin/',
+  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
 }
